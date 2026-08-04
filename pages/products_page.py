2059c5bc1ac7dfc9306b06ff563f218b4644445e
Denis Fernando <denisferoli@gmail.com>
2026-08-03 22:41:01 -0300

    Update products_page.py

--- pages/products_page.py
+++ pages/products_page.py
@@ -14,6 +14,15 @@ class ProductsPage(BasePage):
     cart_item_name = (By.CLASS_NAME, 'inventory_item_name')
     cart_remove_button = (By.ID, 'remove-sauce-labs-backpack')
 
+    products_list = [
+        "add-to-cart-sauce-labs-backpack",
+"add-to-cart-sauce-labs-bike-light",
+"add-to-cart-sauce-labs-bolt-t-shirt",
+"add-to-cart-sauce-labs-fleece-jacket",
+"add-to-cart-sauce-labs-onesie",
+"add-to-cart-test.allthethings()-t-shirt-(red)"
+    ]
+
     def __init__(self, driver):
         super().__init__(driver)
 
